7e34fe1d789d4a1c35950ad328afa474b8f3f20d
Haletran <bpasquier123@gmail.com>
2024-03-27 17:22:22 +0100

    Fix heredoc if over 16 << and error message

--- src/built-ins/heredoc.c
+++ src/built-ins/heredoc.c
@@ -6,25 +6,50 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 13:42:19 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/03/27 12:41:32 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/03/27 17:18:09 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	ft_heredoc(char **str)
+int check_number_of_heredoc(char **input)
 {
-	char	*input;
-	int		count;
-	pid_t	pid;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (input[i])
+	{
+		if (!ft_strncmp(input[i], "<<", 2))
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+int ft_heredoc(char **str)
+{
+	char *input;
+	int i;
+	pid_t pid;
+	int count;
 
 	count = 0;
+	i = 0;
 	if (!str[1])
 	{
 		printf("heredoc: syntax error\n");
 		g_value = 2;
 		return (ERROR);
 	}
+	if (check_number_of_heredoc(str) >= 16)
+	{	
+		printf("minishell : maximum here-document count exceeded\n");
+		return (ERROR);
+	}
+	else
+		i = check_number_of_heredoc(str) + 1;
 	pid = fork();
 	if (pid == -1)
 		return (ERROR);
@@ -40,8 +65,8 @@ int	ft_heredoc(char **str)
 				printf("warning: here-document at line %d delimited by end-of-file (wanted %s)\n", count, str[1]);
 				exit(1);
 			}
-			if (!ft_strncmp(input, str[1], ft_strlen(str[1]))
-				&& ft_strlen(str[1]) == ft_strlen(input))
+			if (!ft_strncmp(input, str[i], ft_strlen(str[i]))
+				&& ft_strlen(str[i]) == ft_strlen(input))
 			{
 				free(input);
 				break ;
