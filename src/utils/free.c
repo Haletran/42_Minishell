9a30754ac55f0b1bd570c8156cc99143d931dc5f
Haletran <bpasquier123@gmail.com>
2024-04-01 17:06:19 +0200

    update files

--- src/utils/free.c
+++ src/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 10:30:09 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/04/01 15:13:41 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/04/01 15:49:01 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	free_tab(char **str)
 		return ;
 	i = 0;
 	size = get_nbargs(str);
-	printf("size = %d\n", size);
 	while (i < size)
 	{
 		free(str[i]);
