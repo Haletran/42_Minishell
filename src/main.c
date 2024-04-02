d9c95af002f89700dfede83df6ba095d2911c6e5
Haletran <bpasquier123@gmail.com>
2024-04-02 23:26:54 +0200

    Clear main code

--- src/main.c
+++ src/main.c
@@ -6,11 +6,36 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:19:09 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/04/02 17:57:00 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/04/02 23:07:37 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+/**
+ * @brief Render prompt for while loop
+ *
+ * @param args
+ */
+void	render_prompt(t_lst **args, char **commands)
+{
+	char	*input;
+
+	handle_sig(0);
+	input = readline("MINISHELL $> ");
+	if (!input)
+	{
+		g_value = 0;
+		free(input);
+		input = NULL;
+		printf("exit\n");
+		exit(g_value);
+	}
+	add_history(input);
+	choose(input, commands, args);
+	free(input);
+	input = NULL;
+}
 
 /**
  * @Main function
@@ -24,33 +49,16 @@
  */
 int	main(int ac, char **av, char **envp)
 {
-	char	*input;
-	char	**commands;
 	t_lst	**args;
+	char	**commands;
 
 	commands = NULL;
 	(void)av;
-	if (ac > 1)
+	if (ac != 1)
 		return (0);
 	args = malloc(sizeof(t_lst));
-	*args = malloc(sizeof(t_lst *) * 10);
 	init_lst(args, envp);
 	while (1)
-	{
-		handle_sig(0);
-		input = readline("MINISHELL $> ");
-		if (!input)
-		{
-			g_value = 0;
-			free(input);
-			input = NULL;
-			printf("exit\n");
-			exit(g_value);
-		}
-		add_history(input);
-		choose(input, commands, args);
-		free(input);
-		input = NULL;
-	}
+		render_prompt(args, commands);
 	return (0);
 }
--- src/parse/init.c
+++ src/parse/init.c
@@ -6,7 +6,7 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 09:54:02 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/04/02 19:29:56 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/04/02 22:41:38 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@
  * @param args
  */
 void	init_lst(t_lst **args, char **envp)
-{
+{	
+	*args = malloc(sizeof(t_lst *) * 12);
 	(*args)->home_path = getenv("HOME");
 	(*args)->username = getenv("USER");
 	(*args)->env_path = getenv("PATH");
@@ -28,7 +29,6 @@ void	init_lst(t_lst **args, char **envp)
 	init_stack(((*args)->env_var_lst), envp);
 	init_stack(((*args)->env_cpy_lst), envp);
 	ft_cpy((*args)->env_var, envp);
-	// ft_cpy((*args)->env_cpy, envp);
 	//(*args)->prompt = ft_join((*args)->username, "$> ");
 	(*args)->exit_code = 0;
 }
