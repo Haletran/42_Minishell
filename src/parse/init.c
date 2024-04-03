97429a6cdefcdfea933a3a9ed2080224eb542769
Haletran <bpasquier123@gmail.com>
2024-04-03 10:55:26 +0200

    Update main branch

--- src/parse/init.c
+++ src/parse/init.c
@@ -6,7 +6,7 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 09:54:02 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/04/02 22:41:38 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/04/03 10:53:22 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
  * @param args
  */
 void	init_lst(t_lst **args, char **envp)
-{	
+{
 	*args = malloc(sizeof(t_lst *) * 12);
 	(*args)->home_path = getenv("HOME");
 	(*args)->username = getenv("USER");
