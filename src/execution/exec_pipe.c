cd6119704b01219a86d245d477181c901eb0867a
Haletran <bpasquier123@gmail.com>
2024-05-04 15:12:39 +0000

    remove debug print

--- src/execution/exec_pipe.c
+++ src/execution/exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 09:54:32 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/05/04 15:06:54 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/05/04 15:12:37 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int exec_pipe(t_cli *cli)
 {
 	(void)cli;
-	print_all_in_cli(cli);
-	print_all_com(cli->com);
+	printf("piping");
+	//print_all_in_cli(cli);
+	//print_all_com(cli->com);
 	return (SUCCESS);
 }
